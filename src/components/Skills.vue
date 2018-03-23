d48d90ec64a198e38fef25c72c746f4e2994b17f
Harman Pannu <iharmanpannu@gmail.com>
2018-03-23 13:26:46 -0700

    finished with app

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -11,10 +11,9 @@
 
   <ul>
 
-<transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bouceOutDown" >
+<transition-group name="list" enter-active-class="animated bounceInUp" leave-active-class="animated bounceOutDown" >
     <li v-for="(data , index) in skills" :key="index" >{{data.skill}}
-
-      <i class="fas fa-minus-circle" v-on:click="remove(index)"></i>
+   <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
        </li>
 </transition-group>
 
@@ -55,6 +54,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import 'https://cdn.jsdelivr.net/npm/animate.css@3.5.2/animate.min.css';
+@import 'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css';
 
 .holder {
   background: #fff;
@@ -102,5 +102,6 @@ input {
 
 i {
   float: right;
+  cursor: pointer;
 }
 </style>
