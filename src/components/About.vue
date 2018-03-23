5b565e888711d2cdf8760753f2a5c66fedaedbce
Harman Pannu <iharmanpannu@gmail.com>
2018-03-23 13:33:29 -0700

    changes to aboout

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-      <h1>Hello {{ their_name }}, this is an about page</h1>
+      <h1>Hello {{ their_name }}, This is an about page</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis repellat recusandae adipisci dolorem molestiae vitae facere hic laboriosam sint delectus! Ullam exercitationem incidunt officiis eveniet odit laborum obcaecati quas debitis.</p>
   </div>
 </template>
